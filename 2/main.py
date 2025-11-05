9c72cf471956600ff52bd94fbd74373a6ef04d20
Tomasz Fertacz <tomaszfertacz@Mac.home>
2025-11-05 22:09:36 +0100

    2

--- 2/main.py
+++ 2/main.py
@@ -1,3 +1,4 @@
+#1
 class Logger:
     def __enter__(self):
         print('Start sekcji logowania')
@@ -5,6 +6,22 @@ class Logger:
     def __exit__(self, *args):
         print('Koniec sekcji logowania')
 
-
 with Logger() as logger:
     print('Środek')
+
+#2
+class FileWriter:
+    def __init__(self, path):
+        self.path = path
+        self.handle = None
+
+    def __enter__(self):
+        self.handle = open(self.path, 'w')
+        return self.handle
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.handle:
+            self.handle.close()
+
+with FileWriter('text.txt') as f:
+    f.write('Tomek')
